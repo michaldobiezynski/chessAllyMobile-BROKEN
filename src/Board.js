98d22abfae022c2648775aeba200d4574a8b370d
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-03-29 20:56:37 +0100

    first commit

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import Background from "./Background";
+
+function Board() {
+  return (
+    <View>
+      <Background />
+    </View>
+  );
+}
+
+export default Board;
