d36af27561e5fea0167c4416c4f8aecfb28c3262
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-03-30 10:24:03 +0100

    make the board component and make it use the background component

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,35 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useCallback, useRef, useState } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import { Chess } from "chess.js";
+
 import Background from "./Background";
 
-function Board() {
+const { width } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height: width,
+  },
+});
+
+const Board = () => {
+  const chess = new Chess();
+  const [state, setState] = useState({
+    player: "w",
+    board: chess.board(),
+  });
+  const onTurn = useCallback(() => {
+    setState({
+      player: state.player === "w" ? "b" : "w",
+      board: chess.board(),
+    });
+  }, [chess, state.player]);
   return (
-    <View>
+    <View style={styles.container}>
       <Background />
     </View>
   );
-}
+};
 
 export default Board;
